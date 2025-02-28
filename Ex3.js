2185e9cbae89a4b1c0ad448f1fe969e7da864c95
luiz-eduardo-ramirez <69591674+luiz-eduardo-ramirez@users.noreply.github.com>
2025-02-28 14:50:35 -0300

    Implementacoes Ex3

--- Ex3.js
+++ Ex3.js
@@ -1,34 +1,71 @@
-/*Exercício 3 7DaysOfCode - JavaScript*/
+/* Exercício 3 7DaysOfCode - JavaScript */
 
-//Escolher área
+// Escolher área
 function escolherArea() {
   let area = "";
-  const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+  const opcoesValidas = ["front-end", "back-end"]; // Lista com as opções aceitas
 
-  while (!regex.test(area.trim())) {
+  while (true) {
     area = prompt("Você prefere front-end ou back-end? (front-end/back-end)");
 
-    if (!regex.test(nome.trim())) {
-      alert("Esolha uma das duas opções");
+    if (area) {
+      area = area.toLowerCase().trim(); // Normaliza a entrada
+      if (opcoesValidas.includes(area)) {
+        return area; // Sai do loop e retorna a escolha válida
+      }
     }
-    return area.toLowerCase().trim(); // Normaliza a resposta para evitar erros
+    alert("Escolha uma opção válida: front-end ou back-end.");
   }
 }
 
-//Escolher linguagem
-function escolherLinguagem() {
+// Escolher linguagem
+function escolherLinguagem(area) {
   let linguagem = "";
   const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+
   if (area === "front-end") {
-    while (!regex.test(linguagem.trim())) {
+    do {
       linguagem = prompt("Você prefere aprender React ou Vue?");
-    }
-    return linguagem.toLowerCase().trim(); // Normaliza a resposta para evitar erros
+    } while (!linguagem || !regex.test(linguagem.trim()));
+  } else if (area === "back-end") {
+    do {
+      linguagem = prompt("Você prefere aprender C# ou Java?");
+    } while (!linguagem || !regex.test(linguagem.trim()));
   }
-  if (area === "back-end") {
-    while (!regex.test(linguagem.trim())) {
-      linguagem = prompt(" Vocé prefere aprender C# ou Java?");
-    }
-    return linguagem.toLowerCase().trim(); // Normaliza a resposta para evitar erros
+  return linguagem.toLowerCase().trim();
+}
+
+// Escolher especialização
+function escolherEspecializacao() {
+  let especializacao = "";
+  const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+
+  do {
+    especializacao = prompt(
+      "Gostaria de se especializar em uma das duas áreas ou em FullStack?"
+    );
+  } while (!especializacao || !regex.test(especializacao.trim()));
+
+  return especializacao.toLowerCase().trim();
+}
+
+// Lista ligada de tecnologias
+function especialidadesTecnologia() {
+  let opcoeLinguagem = true;
+  while (opcoeLinguagem) {
+    let linguagemProgramacao = prompt(
+      "Quais outras linguagens você gostaria de aprender para se especializar? "
+    );
+    alert("Você optou em se especializar em " + linguagemProgramacao + ".");
+
+    opcoeLinguagem =
+      prompt(
+        "Gostaria de Escolher mais alguma? Digite S para sim e N para sair do jogo."
+      ) === S;
   }
 }
+
+escolherArea();
+escolherLinguagem();
+escolherEspecializacao();
+especialidadesTecnologia();
